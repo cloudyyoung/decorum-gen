98a733fd13b86d877e0adb0b9f1bd4070bb81c47
Yunfan Yang <cloudy.young@outlook.com>
2024-10-05 15:08:23 -0600

    Update all_the_same.py

--- decorum_generator/conditions/all_the_same.py
+++ decorum_generator/conditions/all_the_same.py
@@ -1,3 +1,4 @@
+from decorum_generator.conditions.condition import ConditionGroup
 from decorum_generator.conditions.conditions_generator import ConditionsGenerator
 
 
@@ -7,23 +8,25 @@ class AllTheSame(ConditionsGenerator):
             no_room_objects = room.count_objects()
             no_room_wall_colors = 1 if room.wall_color else 0
 
+            # Object colors and styles
+            group = ConditionGroup()
+
             if room.is_identical_object_colors() and room.is_identical_object_styles():
                 condition_str = (
                     f"Objects in the {room} must all have the same color and style."
                 )
-                self.add_condition(condition_str, 2 + no_room_objects)
+                group.append(condition_str, 2 + no_room_objects)
 
             if room.is_identical_object_colors():
                 condition_str = f"Objects in the {room} must all have the same color."
-                self.add_condition(condition_str, 1 + no_room_objects)
+                group.append(condition_str, 1 + no_room_objects)
 
             if room.is_identical_object_styles():
                 condition_str = f"Objects in the {room} must all have the same style."
-                self.add_condition(condition_str, 1 + no_room_objects)
+                group.append(condition_str, 1 + no_room_objects)
 
             if room.is_identical_colors():
                 condition_str = f"Everything in the {room} must all have the same color (objects and wall color)."
-                self.add_condition(
-                    condition_str,
-                    1 + no_room_objects + no_room_wall_colors,
-                )
+                group.append(condition_str, 1 + no_room_objects + no_room_wall_colors)
+
+            self.add_condition_group(group)
