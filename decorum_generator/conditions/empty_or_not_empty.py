e5d8a6912e255e4d052d304671471317913e2eee
Yunfan Yang <cloudy.young@outlook.com>
2024-10-05 15:40:54 -0600

    Update empty_or_not_empty.py

--- decorum_generator/conditions/empty_or_not_empty.py
+++ decorum_generator/conditions/empty_or_not_empty.py
@@ -4,18 +4,20 @@ from decorum_generator.conditions.conditions_generator import ConditionsGenerato
 class EmptyOrNotEmpty(ConditionsGenerator):
     def generate(self, house) -> None:
         # Individual rooms
+        room_group = self.create_condition_group()
         for room in house.rooms:
             if room.count_objects() > 0:
-                self.add_condition(f"The {room} must not be empty.", 0)
+                room_group.add(f"The {room} must not be empty.", 0)
             else:
-                self.add_condition(f"The {room} must be empty.", 0)
+                room_group.add(f"The {room} must be empty.", 0)
 
         # Room groups
+        room_group_group = self.create_condition_group()
         for room_group in house.room_groups:
             if room_group.count_objects() > 0:
-                self.add_condition(f"The {room_group} must not be empty.", 1)
+                room_group_group.add(f"The {room_group} must not be empty.", 1)
             else:
-                self.add_condition(f"The {room_group} must be empty.", 1)
+                room_group_group.add(f"The {room_group} must be empty.", 1)
 
         # Only empty rooms
         no_empty_rooms = house.count_empty_rooms()
