f6a4373a75aebc860f0eb018ac3c26ba07f63772
Yunfan Yang <cloudy.young@outlook.com>
2024-10-05 16:04:52 -0600

    Update no_of_empty_rooms.py

--- decorum_generator/conditions/no_of_empty_rooms.py
+++ decorum_generator/conditions/no_of_empty_rooms.py
@@ -1,19 +1,23 @@
 from decorum_generator.conditions.conditions_generator import ConditionsGenerator
+from decorum_generator.constants.quantifiers import Quantifiers
 
 
 class NoOfEmptyRooms(ConditionsGenerator):
-    def generate(self, house) -> None:
-        no_empty_rooms = house.count_empty_rooms()
+    def generate(self) -> None:
+        no_empty_rooms = self.house.count_empty_rooms()
+
         if no_empty_rooms > 1:
-            quantifier = self.get_random_quantifier()
-            condition_str = (
-                f"The house must contain {quantifier} {no_empty_rooms} empty rooms."
-            )
-            self.add_condition(condition_str, 2)
+            group = self.create_condition_group()
+            for quantifier in list(Quantifiers):
+                condition_str = (
+                    f"The house must contain {quantifier} {no_empty_rooms} empty rooms."
+                )
+                group.add(condition_str, 2)
         elif no_empty_rooms == 1:
-            quantifier = self.get_random_quantifier()
-            condition_str = f"The house must contain {quantifier} 1 empty room."
-            self.add_condition(condition_str, 2)
+            group = self.create_condition_group()
+            for quantifier in list(Quantifiers):
+                condition_str = f"The house must contain {quantifier} 1 empty room."
+                group.add(condition_str, 2)
         else:
             condition_str = "The house must not contain any empty rooms."
             self.add_condition(condition_str, 2)
